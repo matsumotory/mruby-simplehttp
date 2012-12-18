14230cb162e5ca325fee61666b161f91f391cbea
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-19 00:10:37 +0900

    example changed

--- example/get.rb
+++ example/get.rb
@@ -1,1 +1,3 @@
-p SimpleHttp.new("127.0.0.1", 80).request("GET", "/index.html", {'User-Agent' => "test-agent"})
+http = SimpleHttp.new("127.0.0.1", 80)
+
+http.get("/index.html", {'User-Agent' => "test-agent"})
